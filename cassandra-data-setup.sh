5e8008e5b4b1a1cd3ace2fdf22f39ca5a7d29cd9
SowjanyaAchar <55071276+SowjanyaAchar@users.noreply.github.com>
2019-12-07 04:41:36 -0700

    Update cassandra-data-setup.sh

--- cassandra-data-setup.sh
+++ cassandra-data-setup.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-#cqlsh -e "SOURCE 'schema.cql'"
-#cqlsh -e "copy iot.buildinginfo from 'buildinginfo.csv' with header=true"
-#cqlsh -e "copy iot.deviceinfo from 'deviceinfo.csv' with header=true"
-
-
 find_cassandra_pods="kubectl get pods -l name=cassandra"
 
 first_running_seed=$($find_cassandra_pods --no-headers | \
@@ -13,13 +8,8 @@ first_running_seed=$($find_cassandra_pods --no-headers | \
     head -1 | \
     awk '{print $1}')
 
-kubectl cp schema.cql $first_running_seed:/home/
-kubectl cp deviceinfo.csv $first_running_seed:/home/
-kubectl cp buildinginfo.csv $first_running_seed:/home/
+kubectl cp schema1.cql $first_running_seed:/home/
 
 
 
-kubectl exec -it $first_running_seed -- bash -c "cqlsh -e \"SOURCE '/home/schema.cql'"\"
-kubectl exec -it $first_running_seed -- bash -c "cqlsh -e \"copy iot.buildinginfo from '/home/buildinginfo.csv' with header=true\""
-kubectl exec -it $first_running_seed -- bash -c "cqlsh -e \"copy iot.deviceinfo from '/home/deviceinfo.csv' with header=true\""
-
+kubectl exec -it $first_running_seed -- bash -c "cqlsh -e \"SOURCE '/home/schema1.cql'"\"
